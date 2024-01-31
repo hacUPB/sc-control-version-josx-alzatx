fee4bfd39f362885fcfdbc35de5febaddcc95f91
josx-alzatx <144248696+josx-alzatx@users.noreply.github.com>
2024-01-31 15:53:00 +0000

    Cambie cosas en el cual debe ser mayor a 0

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
 
     if(number < 0)
     {
-        perror("El núnmero debe ser mayor a cero");
-        exit(1);
+        fprintf(stderr,"El núnmero debe ser mayor a cero"\n);
+        return(1);
     }
     /*Implement the function that calculates the factorial of the input argument*/
     
